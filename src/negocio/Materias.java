497ee51e5551b898c34c3c4d80409f2a369e0453
Zacarías Samaniego <zacariassamaniego@gmail.com>
2017-06-08 00:49:09 -0300

    Se agrega la clase Materias

---
+++ src/negocio/Materias.java
@@ -0,0 +1,15 @@
+package negocio;
+
+import java.util.ArrayList;
+
+public class Materias {
+	private ArrayList<Materia> materias;
+	public Materias(ArrayList<Materia> materias){
+		this.materias = materias;
+	}
+	
+	public ArrayList<Materia> getMaterias() {
+		return materias;
+	}
+	
+}
