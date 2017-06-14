4164d827bc5624eb55a6c493aca60b23c29064a8
Zacarías Samaniego <zacariassamaniego@gmail.com>
2017-06-13 21:17:47 -0300

    Create AulaTest.java

--- src/negocio/AulaTest.java
+++ src/negocio/AulaTest.java
@@ -6,22 +6,18 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class AulaTest {
+	Aula aula = new Aula();
+	
 	@Test
 	public void constructor(){
-		int numero = 1;
-		Aula aula = new Aula(numero);
-		assertEquals(numero, aula.getNumero());
+		assertEquals(2, aula.numero);
 	}
 	
 	@Test
 	public void MateriasSize() {
-		Aula aula = new Aula(1);
 		assertEquals(0,aula.getMaterias().size());
-//		Materia materia = new Materia("Matematica",16, 10,12);
-//		Materia materia = new Materia("Matematica",16, 10,12);
 		aula.agregarMateria(new Materia("Matematica",16, 10,12){});
 		assertEquals(1,aula.getMaterias().size());
-//		assertNull(aula.getMaterias());
 	}
 
 }
