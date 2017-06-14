4b685d8c0a516c97afa2879548fc13d9e177b563
Zacarías Samaniego <zacariassamaniego@gmail.com>
2017-06-13 21:17:16 -0300

    Update Aula.java

--- src/negocio/Aula.java
+++ src/negocio/Aula.java
@@ -1,14 +1,22 @@
 package negocio;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Aula {
-	private int numero; 
-	private ArrayList<Materia> materias;
+	private static final AtomicInteger count = new AtomicInteger(0); 
+	final int numero; 
+	ArrayList<Materia> materias;
+	int horasDisponibles;
+	int finUltimaMateria;
+	boolean estaLLena;
 
-	public Aula(int numero) {
-		this.numero = numero;
+	public Aula() {
+		numero = count.incrementAndGet();
+		this.horasDisponibles = 12;
 		this.materias = new ArrayList<Materia>();
+		this.finUltimaMateria = 8;
+		
 	}
 
 	public ArrayList<Materia> getMaterias() {
@@ -28,8 +36,18 @@ public class Aula {
 
 	public void agregarMateria(Materia materia) {
 		materias.add(materia);
+//		horasDisponibles -= materia.duracion;
+		finUltimaMateria = materia.horaFin;
 	}
-
-	
+	public boolean llena(){
+		return (horasDisponibles > 0)? true:false;
+	}
+//
+//	public static void main(String[] args) {
+//		Aula a1 = new Aula();
+//		Aula a2 = new Aula();
+//		System.out.println(a1.numero);
+//		System.out.println(a2.numero);
+//	}
 
 }
