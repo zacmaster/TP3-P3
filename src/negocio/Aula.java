2744cc18008f0528196f5e37bde59f86a714777f
Zacarias <zacariassamaniego@gmail.com>
2017-06-29 15:41:08 -0300

    Se actualiza AulaTest

--- src/negocio/Aula.java
+++ src/negocio/Aula.java
@@ -16,19 +16,12 @@ public class Aula {
 		
 	}
 
-//	public String verMaterias(){
-//		if(!materias.equals(null)){
-//			for (Materia materia : materias) {
-//				System.out.println(materia.getNombre()+" ");
-//			}
-//		}
-//		return null;
-//	}
 	public int getNumero(){
 		return numero;
 	}
 
 	public boolean agregarMateria(Materia materia) {
+		if(materia == null)return false;
 		if(materia.duracion <= horasDisponibles && finUltimaMateria <= materia.horaInicio){
 			materias.add(materia);
 			horasDisponibles -= materia.duracion;
@@ -41,20 +34,5 @@ public class Aula {
 	public boolean llena(){
 		return (horasDisponibles == 0)? true:false;
 	}
-//	public static void main(String[] args) {
-//		Aula aula = new Aula();
-//		System.out.println(aula.llena());
-//		Materia m1 = new Materia("A", 00, 8, 12);
-//		Materia m2 = new Materia("B", 00, 13, 17);
-//		Materia m3 = new Materia("C", 00, 18, 20);
-//		Materia m4 = new Materia("A", 00, 20, 23);
-//		aula.agregarMateria(m1);
-//		aula.agregarMateria(m2);
-//		aula.agregarMateria(m3);
-//		aula.agregarMateria(m4);
-//		System.out.println(aula.materias);
-//		
-//		
-//	}
 
 }
--- src/negocio/AulaTest.java
+++ src/negocio/AulaTest.java
@@ -1,43 +1,53 @@
 package negocio;
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
-import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AulaTest {	
-	Aula aula = new Aula(1);
+	Aula _aula;
+	Aula _aulaNull;
+	
+	@Before
+	public void construirAulas(){
+		_aula = new Aula(1);
+	}
 	
 	
 	@Test
 	public void MateriasSize() {
-		assertEquals(0,aula.materias.size());
-		aula.agregarMateria(new Materia("Matematica",16, 10,12){});
-		assertEquals(1,aula.materias.size());
+		assertEquals(0,_aula.materias.size());
+		_aula.agregarMateria(new Materia("Matematica",16, 10,12){});
+		assertEquals(1,_aula.materias.size());
+	}
+	
+	@Test 
+	public void agregarMateriaNull(){
+		assertFalse(_aula.agregarMateria(null));
+	}
+	
+	@Test
+	public void agregarMateriaLargaDuracion(){
+		Materia materia = new Materia("A", 00, 8, 23);
+		assertFalse(_aula.agregarMateria(materia));
 	}
 	
 	@Test
 	public void aulaLlena(){
-		Aula a1 = new Aula(1);
-		assertFalse(a1.llena());
-		a1.agregarMateria(new Materia("A", 0, 8, 12));
-		a1.agregarMateria(new Materia("B", 0, 13, 17));
-		a1.agregarMateria(new Materia("C", 0, 18, 22));
-		System.out.println(a1.horasDisponibles);
-		assertTrue(a1.llena());
+		assertFalse(_aula.llena());
+		llenarAula();
+		assertTrue(_aula.llena());
 	}
 	@Test
-	public void agregarMateria(){
-		Materia m1 = new Materia("A", 00, 8, 12);
-		Materia m2 = new Materia("B", 00, 13, 17);
-		Materia m3 = new Materia("C", 00, 18, 20);
-		Materia m4 = new Materia("A", 00, 20, 23);
-		aula.agregarMateria(m1);
-		aula.agregarMateria(m2);
-		aula.agregarMateria(m3);
-		assertFalse(aula.agregarMateria(m4));
-		assertEquals(3, aula.materias.size());
+	public void superPonerMateria(){
+		llenarAula();
+		Materia materia = new Materia("A", 00, 8, 2);
+		assertFalse(_aula.agregarMateria(materia));
+		assertEquals(3, _aula.materias.size());
 	}
-
+	
+	private void llenarAula(){
+		_aula.agregarMateria(new Materia("A", 0, 8, 12));
+		_aula.agregarMateria(new Materia("B", 0, 13, 17));
+		_aula.agregarMateria(new Materia("C", 0, 18, 22));
+	}
 }
